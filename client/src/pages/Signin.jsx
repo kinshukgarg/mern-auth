b7990bc7bfcc570761cfb9e431f15330ed2db322
kinshukgarg <37059829+kinshukgarg@users.noreply.github.com>
2024-07-05 00:36:29 -0700

    add redux toolkit and function fixed bugs using redux

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,75 +1,73 @@
 import { useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { signInSuccess, signInStart, signInFailure } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Signin() {
-  const [formData, setFormData] = useState({});
-  const [error,setError]=useState(false)
-  const [loading,setLoading]=useState(false)
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
-  //this function used to be async because we are waiting for data 
 
-  const handleSubmit=  async (e) =>{
-    // with this page wont get refresh 
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    dispatch(signInStart());
     try {
-      setLoading(true);
-      setError(false);
-      const res= await fetch('/api/auth/signin',{
-        method:'POST',
-        headers:{
-          'Content-Type':'application/json',
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      body:JSON.stringify(formData)
-        });
-        const data = await res.json();
-        console.log(data) 
-        setLoading( false)
-        if (data.success === false) {
-          setError(true);
-          return;
-        }
-        navigate('/')
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
     }
-    
   };
- 
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Signin</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-       
         <input
           type="text"
           placeholder="Email"
           id="email"
-          className="bg-slate-100 p-3 rounded-lg "
+          className="bg-slate-100 p-3 rounded-lg"
+          value={formData.email}
           onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Fill password"
           id="password"
-          className="bg-slate-100 p-3 rounded-lg "
+          className="bg-slate-100 p-3 rounded-lg"
+          value={formData.password}
           onChange={handleChange}
         />
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-sm uppercase hover:opacity-95 disabled:opacity-80">
-        {loading ? 'Loading...' : 'Sign in'}
-
+          {loading ? 'Loading...' : 'Sign in'}
         </button>
       </form>
       <div className="flex gap-5 mt-8">
-        <p>dont have an account </p>
+        <p>Dont have an account?</p>
         <Link to="/sign-up">
           <span className="text-blue-500">Signup</span>
         </Link>
-        <div><p className="text-red-700 mt-5">{error && 'Something went wrong!'}</p></div>
+      </div>
+      <div>
+        <p className="text-red-700 mt-5">{error ? error.message : 'Something went wrong!'}</p>
       </div>
     </div>
   );
